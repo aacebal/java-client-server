4f22c58ba3fb564a9c2cde168dfa96c8c2d840a9
Adel Acebal <adelacebal@yahoo.com>
2017-09-02 17:57:43 -0400

    multi threading

--- EchoServer/src/com/adelacebal/Main.java
+++ EchoServer/src/com/adelacebal/Main.java
@@ -14,6 +14,10 @@ public class Main {
 
             while(true) {
                 new Echoer(serverSocket.accept()).start();
+//                Socket socket = serverSocket.accept();
+//                Echoer echoer = new Echoer(socket);
+//                echoer.start();
+
             }
 
         } catch(IOException e) {
