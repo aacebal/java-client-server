5a162f3db47a8b232b6b99e607a669d9634b5400
Adel Acebal <adelacebal@yahoo.com>
2017-09-02 15:52:22 -0400

    multi threading

--- EchoServer/src/com/adelacebal/Main.java
+++ EchoServer/src/com/adelacebal/Main.java
@@ -13,18 +13,7 @@ public class Main {
         try(ServerSocket serverSocket = new ServerSocket(5000)) {
 
             while(true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Client connected");
-
-                BufferedReader input = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()));
-                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-                String echoString = input.readLine();
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+                new Echoer(serverSocket.accept()).start();
             }
 
         } catch(IOException e) {
