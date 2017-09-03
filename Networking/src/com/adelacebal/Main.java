2f652b0b6ef7c3782b032c2fd4ffe5b9610a349b
Adel Acebal <adelacebal@yahoo.com>
2017-09-03 10:36:16 -0400

    converting uri to url

--- Networking/src/com/adelacebal/Main.java
+++ Networking/src/com/adelacebal/Main.java
@@ -1,7 +1,9 @@
 package com.adelacebal;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
 
@@ -9,18 +11,26 @@ public class Main {
         try {
             URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
 
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
+            URL url = uri.toURL();
+            System.out.println("URL = " + url);
+
+//            URI uri = new URI("hello");
+
+
+//            System.out.println("Scheme = " + uri.getScheme());
+//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+//            System.out.println("Authority = " + uri.getAuthority());
+//            System.out.println("User info = " + uri.getUserInfo());
+//            System.out.println("Host = " + uri.getHost());
+//            System.out.println("Port = " + uri.getPort());
+//            System.out.println("Path = " + uri.getPath());
+//            System.out.println("Query = " + uri.getQuery());
+//            System.out.println("Fragment = " + uri.getFragment());
 
         } catch(URISyntaxException e) {
             System.out.println("URI bad syntax: " + e.getMessage());
+        } catch(MalformedURLException e) {
+            System.out.println("URL malformed: " + e.getMessage());
         }
     }
 }
