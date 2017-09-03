941860a8128bf75127e1a63e9be7e362f73660c4
Adel Acebal <adelacebal@yahoo.com>
2017-09-03 12:42:07 -0400

    reading high level apis

--- Networking/src/com/adelacebal/Main.java
+++ Networking/src/com/adelacebal/Main.java
@@ -4,15 +4,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
 
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.connect();
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if (responseCode != 200) {
+                System.out.println("Error reading webpage");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+
+            while((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+//
+//            urlConnection.setDoOutput(true);
+//            urlConnection.connect();
+//
+//            BufferedReader inputStream = new BufferedReader(
+//                    new InputStreamReader(urlConnection.getInputStream()));
+//
+//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+//            for (Map.Entry<String, List<String>> entry: headerFields.entrySet()) {
+//                String key = entry.getKey();
+//                List<String> value = entry.getValue();
+//                System.out.println("-----key = " + key);
+//                for(String string: value) {
+//                    System.out.println("-----value = " + value);
+//                }
+//            }
+
+//            String line = "";
+//            while(line != null) {
+//                line = inputStream.readLine();
+//                System.out.println(line);
+//            }
 
 //            BufferedReader inputStream = new BufferedReader(
 //                    new InputStreamReader(url.openStream()));
